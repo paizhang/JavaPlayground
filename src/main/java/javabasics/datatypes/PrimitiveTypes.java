db1dbec0e031204e86e8260ba0c49da7376d2346
paizhang <zhangpai91@gmail.com>
2019-12-07 13:40:50 -0800

    Add max and min range of byte and short primitive type.

--- src/main/java/javabasics/datatypes/PrimitiveTypes.java
+++ src/main/java/javabasics/datatypes/PrimitiveTypes.java
@@ -2,6 +2,9 @@ package javabasics.datatypes;
 
 public class PrimitiveTypes {
 
+    /*
+        int primitive type occupy 32 bits. Range from -2147483648 to 2147483647.
+     */
     public void aboutIntegers() {
         int max = Integer.MAX_VALUE;
         int min = Integer.MIN_VALUE;
@@ -15,5 +18,27 @@ public class PrimitiveTypes {
         System.out.println("Busted number for min is:" + (min - 1));
 
         System.out.println("My max number:" + myMaxNum);
+    }
+
+    /*
+        A byte primitive type occupy 8 bits. Range from -128 to 127.
+     */
+    public void aboutByte() {
+        byte min = Byte.MIN_VALUE;
+        byte max = Byte.MAX_VALUE;
+
+        System.out.println("Byte Max number is:" + max);
+        System.out.println("Byte Min number is:" + min);
+    }
+
+    /*
+        short primitive type occupy 16 bits. Range from -32768 to 32767. 
+     */
+    public void aboutShort() {
+        short min = Short.MIN_VALUE;
+        short max = Short.MAX_VALUE;
+
+        System.out.println("Short Max number is:" + max);
+        System.out.println("Short Min number is:" + min);
     }
 }
