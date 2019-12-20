1279aa5e6c496ef03515335b747af2001f1a0ec9
paizhang <zhangpai91@gmail.com>
2019-12-20 10:47:55 -0800

    Add datatype main class

--- src/main/java/javabasics/datatypes/MainClass.java
+++ src/main/java/javabasics/datatypes/MainClass.java
@@ -3,7 +3,10 @@ package javabasics.datatypes;
 public class MainClass {
 
     public static void main(String[] args) {
-        PrimitiveTypes primitiveTypes = new PrimitiveTypes();
-        primitiveTypes.aboutIntegers();
+        // PrimitiveTypes primitiveTypes = new PrimitiveTypes();
+        // primitiveTypes.aboutChar();
+
+        StringType stringType = new StringType();
+        stringType.stringCatenation();
     }
 }
