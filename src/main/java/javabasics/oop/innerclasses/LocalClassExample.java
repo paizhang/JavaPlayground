2f5da240a3d56bb521559aa47fcb7e6a08d8b9f1
paizhang <zhangpai91@gmail.com>
2019-12-20 12:17:30 -0800

    Add example for local class.

---
+++ src/main/java/javabasics/oop/innerclasses/LocalClassExample.java
@@ -0,0 +1,77 @@
+package javabasics.oop.innerclasses;
+
+/*
+    Local classes are classes that are defined in a block, which is a group of zero or more statements between balanced
+    braces. You typically find local classes defined in the body of a method.
+    Refer from: https://docs.oracle.com/javase/tutorial/java/javaOO/localclasses.html
+ */
+public class LocalClassExample {
+
+    static String regularExpression = "[^0-9]";
+
+    public static void validatePhoneNumber(
+            String phoneNumber1, String phoneNumber2) {
+
+        final int numberLength = 10;
+
+        // Valid in JDK 8 and later:
+
+        // int numberLength = 10;
+
+        /*
+            Note:
+            1. A local class has access to the members of its enclosing class. In the previous example, the PhoneNumber
+            constructor accesses the member LocalClassExample.regularExpression.
+            2. Starting in Java SE 8, a local class can access local variables and parameters of the enclosing block
+            that are final or effectively final. A variable or parameter whose value is never changed after it is
+            initialized is effectively final.
+            3. Local classes are similar to inner classes because they cannot define or declare any static members.
+         */
+        class PhoneNumber {
+
+            String formattedPhoneNumber = null;
+
+            PhoneNumber(String phoneNumber){
+                // numberLength = 7;
+                String currentNumber = phoneNumber.replaceAll(
+                        regularExpression, "");
+                if (currentNumber.length() == numberLength)
+                    formattedPhoneNumber = currentNumber;
+                else
+                    formattedPhoneNumber = null;
+            }
+
+            public String getNumber() {
+                return formattedPhoneNumber;
+            }
+
+            // Valid in JDK 8 and later:
+
+//            public void printOriginalNumbers() {
+//                System.out.println("Original numbers are " + phoneNumber1 +
+//                    " and " + phoneNumber2);
+//            }
+        }
+
+        PhoneNumber myNumber1 = new PhoneNumber(phoneNumber1);
+        PhoneNumber myNumber2 = new PhoneNumber(phoneNumber2);
+
+        // Valid in JDK 8 and later:
+
+//        myNumber1.printOriginalNumbers();
+
+        if (myNumber1.getNumber() == null)
+            System.out.println("First number is invalid");
+        else
+            System.out.println("First number is " + myNumber1.getNumber());
+        if (myNumber2.getNumber() == null)
+            System.out.println("Second number is invalid");
+        else
+            System.out.println("Second number is " + myNumber2.getNumber());
+
+    }
+
+    public static void main(String... args) {
+        validatePhoneNumber("123-456-7890", "456-7890");
+    }
+}
