8e45a1c9bdd6fa96448efbc7a27141688261b0ae
paizhang <zhangpai91@gmail.com>
2019-12-24 11:26:21 -0800

    Using try-with-resource in FileReader

--- src/main/java/javaio/fileio/FileReaderTest.java
+++ src/main/java/javaio/fileio/FileReaderTest.java
@@ -10,27 +10,16 @@ public class FileReaderTest {
     public String readFile(String filePath) throws FileNotFoundException, IOException {
         // System.out.println(new File(".").getAbsolutePath());  // Find out what the root of the search path
         File file = new File(filePath);
-        FileReader fileReader = null;
-        StringBuilder sb = new StringBuilder();
-        try {
-            fileReader = new FileReader(file);
 
+        StringBuilder sb = new StringBuilder();
+        /*
+        Using try-with-resource will help automatically manage the resource. We do not need to close the resource
+        explicitly in the finally block which will make the code concisely.
+         */
+        try (FileReader fileReader = new FileReader(file)) {
             int ch;
             while ((ch = fileReader.read()) != -1) {
                 sb.append((char) ch);
-            }
-        } catch (IOException io) {
-            System.out.println("IO exception. ");
-            throw io;
-        } finally {
-            /*
-                We use finally here to close the resource explicitly. In case some exceptions happens, some files
-                remaining unclosed will cause memory leak.
-             */
-            System.out.println("Finally is called.");
-            if (fileReader != null) {
-                System.out.println("File closed!");
-                fileReader.close();
             }
         }
         return sb.toString();
