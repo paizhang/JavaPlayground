c57d767c8bd2cc24b613c50dc600a4b42887d261
paizhang <zhangpai91@gmail.com>
2020-01-05 23:18:57 -0800

    Add example for displaying IP address within each network interface.

--- src/main/java/network/NetworkInterfaceTest.java
+++ src/main/java/network/NetworkInterfaceTest.java
@@ -1,5 +1,6 @@
 package network;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Collections;
@@ -11,6 +12,33 @@ public class NetworkInterfaceTest {
         Good video explaining what network interface is: https://www.youtube.com/watch?v=PYTG7bvpvRI
      */
     public static void main(String[] args) {
+        displayAllInterfacesInformation();
+    }
+
+    static void displayAllInterfacesInformation() {
+        try {
+            Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+            for (NetworkInterface netint : Collections.list(nets))
+                displayInterfaceInformation(netint);
+        } catch (SocketException se) {
+            se.printStackTrace();
+        }
+    }
+
+    /*
+        Display IP address info within each network interface.
+     */
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        System.out.printf("Display name: %s\n", netint.getDisplayName());
+        System.out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            System.out.printf("InetAddress: %s\n", inetAddress);
+        }
+        System.out.printf("\n");
+    }
+
+    private static void displayInterfacesAndSubInterfaces() {
         try {
             Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
 
